411e1804ffc897d2d218caadcfc37aa2cf30967f
Alex Robinson <alyr1481@gmail.com>
2017-06-24 13:55:33 +0100

    Clean up ejs header and footer files

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+var local = 1 //Set to 0 if using C9
+
 var express = require("express"),
     app = express(),
     mongoose = require('mongoose');
-    
+
 // Express Settings
 app.set('view engine', 'ejs');
 
 // Mongoose Settings
 mongoose.connect("mongodb://localhost/al-tech");
 mongoose.Promise = global.Promise;
-    
-    
+
+
 app.get("/",function(req,res){
-   res.render("index"); 
-});    
-    
-    
-    
-    
-app.listen(process.env.PORT, process.env.IP,function(){
-  console.log("SERVER HAS STARTED!");
-});    
-    
+   res.render("index");
+});
+
+
+if (local){
+  app.listen(3000,function(){
+    console.log("SERVER HAS STARTED!");
+  });
+} else{
+  app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("SERVER HAS STARTED!");
+  });
+}    
