d03577b028dde1a0b172bb5218721b8a16241b4c
Alex Robinson <alyr1481@gmail.com>
2017-06-27 22:10:28 +0000

    Tweak

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var local = 1 //Set to 0 if using C9
+var local = 0 //Set to 0 if using C9
 require('dotenv').config();
 
 var express = require("express"),
@@ -18,7 +18,6 @@ var blogRoutes = require('./routes/blogs'),
 // Mongoose Settings
 mongoose.Promise = global.Promise;
 if (local){
-  //mongoose.connect("mongodb://192.168.1.118:32769/al-tech");
   mongoose.connect(process.env.MONGO_LOCAL);
 } else{
   mongoose.connect(process.env.MONGO_REMOTE);
