659f4ad065c9181467958a2e86111c621fb81dcb
Alex Robinson <alyr1481@gmail.com>
2017-08-02 09:04:20 +0000

    Improve Modal Organisation

--- models/users.js
+++ models/users.js
@@ -5,12 +5,14 @@ var UserSchema = new mongoose.Schema({
   username: { type: String, unique: true, required:true },
   email: { type: String, unique: true, required:true },
   password: String,
+  bio: String,
+  avatar: {type: String, default:"https://s3-eu-west-1.amazonaws.com/al-tech-avatars/picture-default.png"},
   createdAt: {type: Date, default: Date.now},
   resetPasswordToken: String,
   resetPasswordExpires: Date,
   verified: {type: Boolean, default: false},
   isAdmin: {type: Boolean, default: false},
-  avatar: {type: String, default:"https://s3-eu-west-1.amazonaws.com/al-tech-avatars/picture-default.png"}
+  isModerator: {type: Boolean, default: false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
