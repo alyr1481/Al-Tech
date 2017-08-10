21af75f991663bec292c80199c50d6d2846f67e1
Alex Robinson <alyr1481@gmail.com>
2017-08-10 14:21:49 +0000

    Edit Button Now Works!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var router  = express.Router({mergeParams: true});
+var Post = require("../models/posts");``
 
 
 router.get("/",function(req,res){
   res.render("landing");
+});
+
+router.get("/home",function(req,res){
+ Post.find({}).populate("postType").exec(function(err,posts){
+  if (err){
+   console.log(err);
+  } else{
+   res.render("home",{posts: posts});
+  }
+ });
 });
 
 module.exports = router;
