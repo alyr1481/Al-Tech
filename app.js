c05c29b98208ba0ad46e835a8c1080263c5f756f
Alex Robinson <alyr1481@gmail.com>
2017-08-17 11:35:19 +0000

    Complete Facebook Linking

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(function(req,res,next){
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
+  res.locals.headers = req.headers;
   next();
 });
 
